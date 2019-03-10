9bd2c53503bb2a505d4d9f9033f7f6a1e6864442
Theo <haoyuan.guo@clearbridgemobile.com>
2019-03-10 09:35:30 -0400

    info_page/save data as sharedpref

--- app/src/main/java/com/prototype/farmCiti/fragments/Entryformfrag.java
+++ app/src/main/java/com/prototype/farmCiti/fragments/Entryformfrag.java
@@ -1,5 +1,7 @@
 package com.prototype.farmCiti.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,14 +11,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fastaccess.datetimepicker.DatePickerFragmentDialog;
 import com.fastaccess.datetimepicker.DateTimeBuilder;
 import com.fastaccess.datetimepicker.callback.DatePickerCallback;
 import com.fastaccess.datetimepicker.callback.TimePickerCallback;
+import com.google.gson.Gson;
 import com.prototype.farmCiti.R;
+import com.prototype.farmCiti.datamodel.MealObject;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -27,7 +33,11 @@ public class Entryformfrag extends Fragment implements DatePickerCallback, TimeP
     private View rootView;
     private Spinner spinner;
     private Button pickTime;
+    private Button saveData;
     private TextView dateResult;
+    private EditText consumptionAmount;
+    private long rawDate;
+    private SharedPreferences sharedPreferences;
 
     public Entryformfrag() {
     }
@@ -41,12 +51,47 @@ public class Entryformfrag extends Fragment implements DatePickerCallback, TimeP
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_entryform, container, false);
         spinner = (Spinner) rootView.findViewById(R.id.food_types_spinner);
+        saveData = (Button) rootView.findViewById(R.id.save_record);
+        consumptionAmount = (EditText) rootView.findViewById(R.id.input_unit);
+        sharedPreferences = getActivity().getSharedPreferences("data", Context.MODE_PRIVATE);
 
         pickTime = (Button) rootView.findViewById(R.id.pick_time);
         pickTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DatePickerFragmentDialog.newInstance(DateTimeBuilder.get().withTheme(R.style.PickersTheme)).show(getChildFragmentManager(), "DatePickerFragmentDialog");
+            }
+        });
+
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String consumptionType = spinner.getSelectedItem().toString().split(" ")[0];
+                if(consumptionAmount == null) {
+                    Toast.makeText(getContext(),"Failed: data incomplete!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                double amountConsumed = Double.parseDouble(consumptionAmount.getText().toString());
+                Date date = new Date();
+                date.setTime(rawDate);
+                if ((!consumptionType.isEmpty() && amountConsumed != 0 && rawDate != 0)) {
+                    MealObject mealObject = new MealObject(consumptionType,amountConsumed,date);
+                    Gson gson = new Gson();
+                    String jsonString = gson.toJson(mealObject);
+                    Toast.makeText(getContext(),jsonString,Toast.LENGTH_SHORT).show();
+                    String existingData = sharedPreferences.getString("JsonTag","");
+                    if(!existingData.isEmpty()) {
+                        jsonString = existingData + "," + jsonString;
+                    }
+                    sharedPreferences.edit().putString("JsonTag",jsonString).apply();
+
+                    consumptionAmount.getText().clear();
+                    rawDate = 0;
+                    dateResult.setVisibility(View.GONE);
+                    Toast.makeText(getContext(),"Data recorded!",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getContext(),"Failed: data incomplete!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -74,6 +119,7 @@ public class Entryformfrag extends Fragment implements DatePickerCallback, TimeP
 
     @Override public void onDateSet(long date) {
         dateResult.setVisibility(View.VISIBLE);
+        rawDate = date;
         dateResult.setText(getDateOnly(date));
     }
 
