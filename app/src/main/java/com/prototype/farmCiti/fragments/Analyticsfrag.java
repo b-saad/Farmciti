34b385f46d844fe560ceb1c807dd20a9bfb32209
Bilal Saad <bilalsaad923@gmail.com>
2019-03-09 22:49:00 -0500

    water graph added

--- app/src/main/java/com/prototype/farmCiti/fragments/Analyticsfrag.java
+++ app/src/main/java/com/prototype/farmCiti/fragments/Analyticsfrag.java
@@ -13,8 +13,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -30,28 +32,29 @@ public class Analyticsfrag extends Fragment {
     // Views
     private View rootView;
     private BarChart mMeatChart;
-    private BarChart mWaterChart;
+    private HorizontalBarChart mWaterChart;
     private TextView mMeatChartCaptionTextView;
     private TextView mWaterChartCaptionTextView;
 
+    // water per gram of meat consumed
     private static float WATER_BEEF = 15.03f; //per gram
     private static float WATER_PORK = 4.8f;
     private static float WATER_CHICKEN = 4.325f;
     private static float WATER_LAMB = 8.763f;
 
+    // C02 per gram of meat consumed
     private static float CO2_BEEF = 0.0346f;
     private static float CO2_PORK = 0.00635f;
     private static float CO2_CHICKEN = 0.00457f;
     private static float CO2_LAMB = 0.0174f;
 
-    List<BarEntry> meatAverageEntries = new ArrayList<BarEntry>();
-    List<BarEntry> meatUserEnteries = new ArrayList<BarEntry>();
-
     String[] values = new String[] {
             "Poultry", "Pigmeat", "Beef", "Mutton/Goat"
     };
 
-    public void addData() {
+    public void addMeatData() {
+        List<BarEntry> meatAverageEntries = new ArrayList<BarEntry>();
+        List<BarEntry> meatUserEnteries = new ArrayList<BarEntry>();
 
         meatAverageEntries.add(new BarEntry(0f, 100.5f));
         meatAverageEntries.add(new BarEntry(1f, 62.4f));
@@ -79,13 +82,44 @@ public class Analyticsfrag extends Fragment {
         XAxis xAxis = mMeatChart.getXAxis();
         xAxis.setCenterAxisLabels(true);
         mMeatChart.getAxisRight().setEnabled(false);
-        xAxis.setValueFormatter(new MyXAxisValueFormatter(values));
+        xAxis.setValueFormatter(new MeatXAxisValueFormatter(values));
         xAxis.setGranularity(1f);
         xAxis.setAxisMinimum(0);
         xAxis.setAxisMaximum(4);
         mMeatChart.fitScreen();
         mMeatChart.invalidate(); // refresh
     }
+
+    public void addWaterData() {
+        List<BarEntry> waterUserEnteries = new ArrayList<BarEntry>();
+
+        waterUserEnteries.add(new BarEntry(0, 4));
+        waterUserEnteries.add(new BarEntry(1, 5));
+        waterUserEnteries.add(new BarEntry(2, 6));
+        waterUserEnteries.add(new BarEntry(3, 7));
+
+        float groupSpace = 0.06f;
+        float barSpace = 0.02f; // x2 dataset
+        float barWidth = 0.45f; // x2 dataset
+        // (0.02 + 0.45) * 2 + 0.06 = 1.00 -> interval per "group"
+
+        BarDataSet dataSet = new BarDataSet(waterUserEnteries, "Your Water Usage");
+        dataSet.setColor(Color.BLUE);
+
+        BarData data = new BarData(dataSet);
+        data.setBarWidth(barWidth); // set the width of each bar
+        mWaterChart.setData(data);
+        XAxis xAxis = mWaterChart.getXAxis();
+//        xAxis.setCenterAxisLabels(true);
+        xAxis.setValueFormatter(new WaterXAxisValueFormatter(values));
+        xAxis.setGranularity(1f);
+        xAxis.setAxisMinimum(-0.5f);
+        xAxis.setAxisMaximum(3.5f);
+        mWaterChart.setFitBars(true);
+        mWaterChart.fitScreen();
+        mWaterChart.invalidate(); // refresh
+    }
+
 
     public Analyticsfrag() {
 
@@ -106,17 +140,44 @@ public class Analyticsfrag extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mMeatChart = (BarChart) view.findViewById(R.id.bc_meat_consumption_chart);
-        mWaterChart = (BarChart) view.findViewById(R.id.bc_water_usage_chart);
+        mWaterChart = (HorizontalBarChart) view.findViewById(R.id.bc_water_usage_chart);
         mMeatChartCaptionTextView = (TextView) view.findViewById(R.id.tv_meat_consumption_caption);
         mWaterChartCaptionTextView = (TextView) view.findViewById(R.id.tv_water_usage_caption);
-        addData();
+        addMeatData();
+        addWaterData();
     }
 
-    public class MyXAxisValueFormatter implements IAxisValueFormatter {
+    public class MeatXAxisValueFormatter implements IAxisValueFormatter {
 
         private String[] mValues;
 
-        public MyXAxisValueFormatter(String[] values) {
+        public MeatXAxisValueFormatter(String[] values) {
+            this.mValues = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            // "value" represents the position of the label on the axis (x or y)
+            if ( value <= 0 ) {
+                return mValues[0];
+            } else if ( value == 1) {
+                return mValues[1];
+            } else if ( value == 2) {
+                return mValues[2];
+            } else if ( value == 3) {
+                return mValues[3];
+            }
+            return "";
+        }
+
+    }
+
+
+    public class WaterXAxisValueFormatter implements IAxisValueFormatter {
+
+        private String[] mValues;
+
+        public WaterXAxisValueFormatter(String[] values) {
             this.mValues = values;
         }
 
